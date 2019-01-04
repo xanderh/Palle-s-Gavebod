2b2eaad728e248f1542e087ed11dd27dbc026969
Alexander Hvilshøj <alexander@hvilshoej.dk>
2019-01-04 14:03:50 +0100

    Add project files.

---
+++ Palle's Gavebod/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Palle_s_Gavebod.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
